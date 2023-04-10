e3d349d7ed174fd04b5fe88ea0c9cdf01fc0d471
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-04-10 10:38:55 +0700

    metode

--- class_oop_102/class_oop_102.cpp
+++ class_oop_102/class_oop_102.cpp
@@ -5,4 +5,8 @@ class Mahasiswa {			// class
 public:						// akses modifier
 	string nim;
 	string nama;
+public: void cetak() {		//metode
+	cout << "NIM = " << nim;
+	cout << "\nNama = " << nama;
+}
 };
