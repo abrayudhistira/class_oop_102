9209ba5471d95cb7fa37c4301f57fa519342b44c
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-04-10 10:37:43 +0700

    class

--- class_oop_102/class_oop_102.cpp
+++ class_oop_102/class_oop_102.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa {
-public:
-	string nim;
-	string nama;
-public: void cetak() {
-	cout << "NIM = " << nim;
-	cout << "\nNama = " << nama;
-	}
+class Mahasiswa {			// class 
+
 };
