08b62757dcb31fb908fcfa6ca486af1bae1a6234
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-04-10 11:01:11 +0700

    memanggil metode

--- class_oop_102/class_oop_102.cpp
+++ class_oop_102/class_oop_102.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 class Mahasiswa {			// class 
-public:						// akses modifier
+public:						// akses modifier untuk object public
 	string nim;
 	string nama;
-public: void cetak() {		//metode
+public: void cetak() {		// akses modifier, metode untuk public
 	cout << "NIM = " << nim;
 	cout << "\nNama = " << nama;
 	}
 };
 
 class Matakuliah {				//class
-private:						// akses modifier
+private:						// akses modifier untuk object private
 	string kode;
 	string namaM;
 	int sks;
-public:							//akses modifier
-	void input() {								// metode
+public:							//akses modifier untuk object public
+	void input() {								// metode dari akses modifier public
 		cout << "Masukkan kode Mata Kuliah : ";
 		cin >> kode;
 		cout << "Masukkan Nama Mata Kuliah : ";
@@ -25,7 +25,7 @@ public:							//akses modifier
 		cout << "Jumlah SKS : ";
 		cin >> sks;
 	}
-	void tampil() {								// metode
+	void tampil() {								// metode dari akses modifer public
 		cout << "Kode Mata Kuliah : " << kode;
 		cout << "Nama Mata Kuliah : " << namaM;
 		cout << "SKS : " << sks;
@@ -36,4 +36,12 @@ int main()									// main program
 {
 	Mahasiswa mhs1;
 	Matakuliah mk;
+
+	cout << "Masukkan NIM : ";				// memanggil metode
+	cin >> mhs1.nim;
+	cout << "Masukkan Nama : ";
+	cin >> mhs1.nama;
+	mhs1.cetak();
+
+
 }
